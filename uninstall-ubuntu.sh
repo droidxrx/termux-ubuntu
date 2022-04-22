93c74983397114e1da56d69b98d323475403df61
DroidXrX <61324913+droidxrx@users.noreply.github.com>
2022-04-22 22:08:06 +0700

    Create uninstall-ubuntu.sh

---
+++ uninstall-ubuntu.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "Starting to uninstall, please be patient..."
+
+cur=`pwd`
+if [ -d "$HOME/.local" ]; then
+    if [ -d "$HOME/.local/share" ]; then
+        if [ -d "$HOME/.local/share/ubuntu" ]; then
+            cd $HOME/.local/share/ubuntu
+            chmod 777 -R ubuntu
+            rm -rf ubuntu
+        fi
+    fi
+fi
+cd $cur
+rm -rf uninstall-ubuntu.sh
+
+echo "Done"
